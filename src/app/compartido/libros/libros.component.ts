6cbda8f3de089187f6bb44e366ec6c1e0eed5454
daya-pao <dayana06052002@gmail.com>
2024-02-22 20:02:39 -0500

    varios cambios

---
+++ src/app/compartido/libros/libros.component.ts
@@ -0,0 +1,45 @@
+import { Component ,OnInit, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { literalMap } from '@angular/compiler';
+
+
+@Component({
+  selector: 'app-libros',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './libros.component.html',
+  styleUrl: './libros.component.css'
+})
+export class LibrosComponent implements OnInit{
+
+  libros: any[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchLibros();
+  }
+
+  fetchLibros() {
+    this.httpClient.get('https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=10&key=AIzaSyCYJ8IOND5esjZx-IlnV2YuhVRPfwyH0wg')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.libros = data.items; 
+      });
+  }
+
+
+  /* Busqueda: string = '';
+  Categoriaselecionada: string = '';
+  librosFiltro: any[] = [];
+  
+
+  onSearchClick() {
+    this.librosFiltro = this.libros.filter(libro =>
+      libro.titulo.toLowerCase().includes(this.Busqueda.toLowerCase()) &&
+      (this.Categoriaselecionada === '' || libro.categoria.toLowerCase() === this.Categoriaselecionada.toLowerCase())
+    );
+  };  */
+}
